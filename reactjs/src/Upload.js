e111d937d87555a53b8fe87f67f0ece24df46d2d
s <ste.luong@gmail.com>
2019-11-12 09:25:00 +0100

    Form added

--- reactjs/src/Upload.js
+++ reactjs/src/Upload.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button,Grid, Row, Col,Table } from 'react-bootstrap';
+import { Button,Grid, Row, Col,Table, Form } from 'react-bootstrap';
 class Upload extends Component {
     constructor() {
         super();
@@ -19,43 +19,52 @@ class Upload extends Component {
     send(e){
         e.preventDefault();
         console.log("submit");
+        //console.log(e.target.form.location.value);
         const formData = new FormData();
-        formData.append('screen', this.state.file);
-        this.setState({
-            status:"uploading file ..."
-        });
-        fetch("http://slapps.fr:8088/uploads", {
-            method: 'POST',
-            body: formData
-        })
-        .then(res=>{
+        var location = e.target.form.location.value;
+        var comment = e.target.form.comment.value;
+        if(!this.state.file){
             this.setState({
-                status:"... Uploaded"
+                status:"No file detected"  
             });
-            return res.json()
-        }).then(data=>{
-            console.log(data);
-            var path = data.path;
-            var params = {
-                path : path,
-                date : new Date(),
-                location : "test",
-                comment : "test"
-            };
-            fetch("http://slapps.fr:3001/api/receipts",{
+        }else{
+            formData.append('screen', this.state.file);
+            this.setState({
+                status:"uploading file ..."
+            });
+            fetch("http://slapps.fr:8088/uploads", {
                 method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(params)
-            }).then(res=>{
-                this.setState({
-                    status:"... Uploaded and Saved" 
-                });   
-
+                body: formData
             })
-        })
+            .then(res=>{
+                this.setState({
+                    status:"... Uploaded"
+                });
+                return res.json();
+            }).then(data=>{
+                console.log(data);
+                var path = data.path;
+                var params = {
+                    path : path,
+                    date : new Date(),
+                    location : location,
+                    comment : comment
+                };
+                fetch("http://slapps.fr:3001/api/receipts",{
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(params)
+                }).then(res=>{
+                    this.setState({
+                        status:"... Uploaded and Saved" 
+                    });   
+
+                })
+            })
+        }
 
 
     }
@@ -64,10 +73,15 @@ class Upload extends Component {
         return(
                 <div>
                 {this.state.status===""?"":<p>{this.state.status}</p>}
-                <form>
+                <Form>
                 <input type="file" onChange={(e)=>this.load(e)}/>
+                <br/>
+                Location - <input type="text" name="location"/>
+                <br/>
+                Comment - <input type="text" name="comment"/>
+                <br/>
                 <Button type="submit" onClick={(e)=>this.send(e)}>Submit</Button>
-                </form>
+                </Form>
                 </div>
               )
     }
