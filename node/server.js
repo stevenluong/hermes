392405cee54deaf634356d36465312c80072fa32
Steven Luong <stevenluong@Stevens-MacBook-Air.local>
2019-11-12 11:25:39 +1100

    return path of uploaded file

--- node/server.js
+++ node/server.js
@@ -7,7 +7,7 @@ var app = express();
 app.post('/uploads', upload.single('screen'), function (req, res, next) {
     console.log(req.file);
     console.log(req.body);
-    res.send("OK");
+    res.send(req.path);
 })
 
 app.listen(8088);
